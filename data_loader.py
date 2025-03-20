e92f249a56bfcb2e85f914eba4c678b74724c21a
a1exuus <162327737+a1exuus@users.noreply.github.com>
2025-03-20 17:52:45 +0300

    show only active pokemons and connected model to pokemon page

--- data_loader.py
+++ data_loader.py
@@ -3,15 +3,18 @@ import collections
 import argparse
 import sys
 
+
 def get_input_data():
     parser = argparse.ArgumentParser(description='Данный скрипт структурирует меню и возраст сайта и сразу же вносит изменения в файл сайта')
     parser.add_argument('-path', '-path_to_xlsx_file', help='Путь до .xlsx файла вашего меню', type=str)
     parser.add_argument('-command', choices=['runserver'], help='Команды для скрипта')
     return parser.parse_args(sys.argv[1:]).path
 
+
 def load_wine_data(path_to_xlsx):
     wines = pandas.read_excel(f'{path_to_xlsx}.xlsx', na_values='None', keep_default_na=False).T.to_dict()
     return wines
+
 
 def group_production(production):
     output = collections.defaultdict(list)
@@ -19,6 +22,7 @@ def group_production(production):
         if product:
             output[product['Категория']].append(product)
     return output
+
 
 def get_year_word_form(years: int) -> str:
     if 11 <= years % 100 <= 19:
--- main.py
+++ main.py
@@ -3,15 +3,18 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 from datetime import date
 from data_loader import load_wine_data, group_production, get_year_word_form, get_input_data
 
+
 def generate_html(years, wines, template):
     rendered_page = template.render(years=get_year_word_form(years), wines=wines)
     with open('index.html', 'w', encoding='utf8') as file:
         file.write(rendered_page)
 
+
 def run_server():
     server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
     print("Сервер запущен на http://0.0.0.0:8000")
     server.serve_forever()
+
 
 def main():
     current_year = date.today().year
@@ -31,6 +34,7 @@ def main():
     generate_html(years, grouped_wines, template)
     
     run_server()
+
 
 if __name__ == '__main__':
     main()
